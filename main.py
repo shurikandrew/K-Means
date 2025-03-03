e65738e039d43a466b911803680f67827deeeefa
shurikandrew <s26865@pjwstk.edu.pl>
2025-03-03 21:45:10 +0100

    project

---
+++ main.py
@@ -0,0 +1,109 @@
+import random
+
+k = int(input("Please, provide number of clusters: "))
+observationFile = input("Please, provide path to the file: ")
+print("------------------------------------------")
+valuesList = []
+with open(observationFile, "r") as file:
+    fileLines = file.readlines()
+    tempList = []
+    for list in fileLines:
+        tempList = list.split(',')
+        tempList[len(tempList)-1] = tempList[len(tempList)-1].replace('\n', '')
+        for index in range(len(tempList)-1):
+            tempList[index] = float(tempList[index])
+        valuesList.append(tempList)
+
+amountOfAttributes = len(valuesList[0])-1
+
+clusters = []
+centroidList = []
+
+for i in range(0, k):
+    clusters.append([])
+
+for observation in valuesList:
+    clusters[random.randint(0,k-1)].append(observation)
+
+
+def getCluster(observation):
+    for cluster in clusters:
+        if(observation in cluster):
+            return clusters.index(cluster)
+
+def getPurity():
+    print("Purity:")
+    for clusterIndex in range(len(clusters)):
+        classes = {}
+        for observation in clusters[clusterIndex]:
+            if observation[len(observation)-1] not in classes:
+                classes[observation[len(observation) - 1]] = 0
+            classes[observation[len(observation)-1]] += 1
+        print(f"\tCluster {clusterIndex+1}:")
+        for key in classes:
+            print(f"\t\t{key}: {classes[key]/len(clusters[clusterIndex])*100}%")
+
+
+def calculateCentroids():
+    global centroidList
+
+    centroidList = []
+
+    for _ in range(k):
+        centroidList.append([0 for _ in range(amountOfAttributes)])
+
+    for clusterIndex in range(len(clusters)):
+        try:
+            for observationIndex in range(len(clusters[clusterIndex])):
+                for index in range(len(clusters[clusterIndex][observationIndex]) - 1):
+                    centroidList[clusterIndex][index] += clusters[clusterIndex][observationIndex][index]
+
+
+            for index in range(len(centroidList[clusterIndex])):
+                centroidList[clusterIndex][index] /= len(clusters[clusterIndex])
+
+
+        except ZeroDivisionError:
+            pass
+    
+
+changed = True
+
+def assignClusters():
+    global clusters
+    global changed
+
+    changed = False
+
+    tempClusters = []
+    for i in range(0, k):
+        tempClusters.append([])
+
+
+    sumDistances = 0
+    for observation in valuesList:
+        resultList = []
+        previousCluster = getCluster(observation)
+        for centroid in centroidList:
+            result = 0
+            for index in range(len(centroid)):
+                result += (centroid[index] - observation[index])**2
+            resultList.append(result)
+        minDistance = min(resultList)
+        sumDistances += minDistance
+        nearestCluster = resultList.index(minDistance)
+        tempClusters[nearestCluster].append(observation)
+        if(previousCluster != nearestCluster):
+            changed = True
+    clusters = tempClusters
+
+    print(f"Sum of distances: {sumDistances}")
+    getPurity()
+    print("------------------------------------------")
+
+
+
+while(changed):
+    calculateCentroids()
+    assignClusters()
+
